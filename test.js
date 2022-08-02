ef503d940400c0f9c5c2f8e63cf6ab438f1b93d2
Piotr Rajmanski <prajmanski10@gmail.com>
2022-08-02 11:37:42 +0200

    Else If Statements

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
-const youLikeMeat = false;
+const myAge = 19;
 
-if (youLikeMeat) {
-    document.write('Here is a meat menu....');
+if (myAge > 30) {
+    document.write('You are over 30!');
+} else if (myAge >= 20) {
+    document.write('You are ober 20!');
 } else {
-    document.write('You dont like meat so here is a vegan menu....');
+    document.write('You are below 20!')
 }
