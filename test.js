9f9463f9b33fc241ba3e43258f2591716b217648
Piotr Rajmanski <prajmanski10@gmail.com>
2022-08-04 11:04:44 +0200

    The onClick Event

--- test.js
+++ test.js
@@ -1,14 +1,12 @@
-//Date object
+const content = document.getElementById('content');
+const button = document.getElementById('show-more');
 
-const myDate = new Date();
-console.log(myDate);
-
-const myPastDate = new Date(1545, 11, 2);
-const myFutureDate = new Date(2515, 12, 01);
-console.log(myPastDate);
-console.log(myFutureDate);
-
-const birthday = new Date(1991, 6, 1);
-console.log(birthday.getMonth());
-console.log(birthday.getFullYear());
-console.log(birthday.getTime());
+button.onclick = function() {
+    if(content.className == 'open') {
+        content.className = '';
+        button.innerHTML = 'Show more';
+    } else {
+        content.className = 'open';
+        button.innerHTML = 'Show Less';
+    }
+};
